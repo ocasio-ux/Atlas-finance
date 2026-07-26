f2f442df45a78f4ed34c8b5a9fbc2009673f96bd
ocasio-ux <igor.guiguiu@gmail.com>
2026-07-25 23:35:09 -0300

    feat: add Firebase Google authentication service

---
+++ lib/core/auth/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// Account identity layer for Atlas.
+/// Google credentials are handled by Google/Firebase, never stored by Atlas.
+class AuthService {
+  AuthService({FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  final FirebaseAuth _firebaseAuth;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn.instance.authenticate();
+    final googleAuth = googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
+
+    return _firebaseAuth.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+    await GoogleSignIn.instance.signOut();
+  }
+}
