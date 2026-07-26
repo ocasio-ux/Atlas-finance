51a85ab89cfb0bb2b05edb5d6be7d89145a29bd4
ocasio-ux <igor.guiguiu@gmail.com>
2026-07-26 00:05:24 -0300

    test: fix Atlas app import and splash-aware flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:atlas_finance/main.dart';
+import 'package:atlas_finance/app/app.dart';
 
 void main() {
-  testWidgets('Atlas opens new transaction flow', (WidgetTester tester) async {
+  testWidgets('Atlas opens splash and new transaction flow', (WidgetTester tester) async {
     await tester.pumpWidget(const AtlasApp());
 
     expect(find.text('ATLAS'), findsOneWidget);
+    expect(find.text('Seu dinheiro. Sob controle.'), findsOneWidget);
+
+    await tester.pump(const Duration(seconds: 3));
+    await tester.pumpAndSettle();
+
     expect(find.text('Saldo total'), findsOneWidget);
     expect(find.text('Adicionar'), findsOneWidget);
 
