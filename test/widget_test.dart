11062b5aa13ed521c9f8160a3cf0912d7fc89860
ocasio-ux <igor.guiguiu@gmail.com>
2026-07-25 23:26:48 -0300

    test: cover Atlas home to transaction flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:atlas_finance/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Atlas opens new transaction flow', (WidgetTester tester) async {
+    await tester.pumpWidget(const AtlasApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('ATLAS'), findsOneWidget);
+    expect(find.text('Saldo total'), findsOneWidget);
+    expect(find.text('Adicionar'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('Adicionar'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Nova movimentação'), findsOneWidget);
+    expect(find.text('Salvar movimentação'), findsOneWidget);
   });
 }
