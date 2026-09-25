db303b7ba434c665f498496f277d2d25d412b496
ocasio-ux <igor.guiguiu@gmail.com>
2026-09-25 17:23:48 -0300

    feat(finance): add monthly transaction recurrence engine

---
+++ lib/core/finance/transaction_recurrence.dart
@@ -0,0 +1,54 @@
+import '../transactions/transaction_model.dart';
+
+class TransactionRecurrenceEngine {
+  const TransactionRecurrenceEngine();
+
+  Iterable<AtlasTransaction> occurrencesBetween(
+    AtlasTransaction template, {
+    required DateTime start,
+    required DateTime end,
+  }) sync* {
+    if (template.repeat == TransactionRepeat.none ||
+        end.isBefore(start)) {
+      return;
+    }
+
+    var occurrenceDate = _addMonths(template.transactionDate, 1);
+    while (!occurrenceDate.isAfter(end)) {
+      if (!occurrenceDate.isBefore(start)) {
+        yield AtlasTransaction(
+          id: '${template.id}_recurrence_${occurrenceDate.year}_${occurrenceDate.month}',
+          type: template.type,
+          amount: template.amount,
+          description: template.description,
+          createdAt: template.createdAt,
+          transactionDate: occurrenceDate,
+          category: template.category,
+          sourceType: template.sourceType,
+          sourceId: template.sourceId,
+          destinationType: template.destinationType,
+          destinationId: template.destinationId,
+          repeat: TransactionRepeat.none,
+          seriesId: template.seriesId ?? template.id,
+          cardInvoiceEndDate: null,
+        );
+      }
+      occurrenceDate = _addMonths(occurrenceDate, 1);
+    }
+  }
+
+  DateTime _addMonths(DateTime date, int months) {
+    final targetMonth = date.month - 1 + months;
+    final year = date.year + targetMonth ~/ 12;
+    final month = targetMonth % 12 + 1;
+    final lastDay = DateTime(year, month + 1, 0).day;
+    return DateTime(
+      year,
+      month,
+      date.day.clamp(1, lastDay),
+      date.hour,
+      date.minute,
+      date.second,
+    );
+  }
+}
